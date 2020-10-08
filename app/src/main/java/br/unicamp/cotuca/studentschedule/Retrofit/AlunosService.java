70978e809d6f14101ee113797ae96ef686ed6a93
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-10-08 09:38:25 -0300

    define retrofit routes

--- app/src/main/java/br/unicamp/cotuca/studentschedule/Retrofit/AlunosService.java
+++ app/src/main/java/br/unicamp/cotuca/studentschedule/Retrofit/AlunosService.java
@@ -1,4 +1,28 @@
 package br.unicamp.cotuca.studentschedule.Retrofit;
 
+import br.unicamp.cotuca.studentschedule.Models.Aluno;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
 public interface AlunosService {
+    @GET("alunos")
+    Call<Aluno[]> buscarAlunos();
+
+    @GET("alunos/{ra}")
+    Call<Aluno> buscarAluno(@Path("ra") String ra);
+
+    @POST("alunos")
+    Call<Aluno> cadastrarAluno(@Body Aluno aluno);
+
+    @PUT("alunos")
+    Call<Aluno> editarAluno(@Body Aluno aluno);
+
+    @DELETE("alunos/{ra}")
+    Call<Aluno> removerAluno(@Body Aluno aluno);
+
 }
