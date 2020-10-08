bc9d9ea43f1d000e7a9cbbb322aba4f7a612bf09
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-10-08 08:13:25 -0300

    first design

--- app/src/main/java/br/unicamp/cotuca/studentschedule/MainActivity.java
+++ app/src/main/java/br/unicamp/cotuca/studentschedule/MainActivity.java
@@ -3,12 +3,19 @@ package br.unicamp.cotuca.studentschedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnGet, btnPost, btnPut, btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGet = findViewById(R.id.btnGet);
+        btnPost = findViewById(R.id.btnPost);
+        btnPut = findViewById(R.id.btnPut);
+        btnDelete = findViewById(R.id.btnDelete);
     }
 }
