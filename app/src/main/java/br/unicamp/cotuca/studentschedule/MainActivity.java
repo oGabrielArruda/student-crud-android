3d27f32e049caa6e6e88b49d1f2815749109e5d4
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-10-08 08:18:06 -0300

    navegacao entre janelas

--- app/src/main/java/br/unicamp/cotuca/studentschedule/MainActivity.java
+++ app/src/main/java/br/unicamp/cotuca/studentschedule/MainActivity.java
@@ -2,7 +2,9 @@ package br.unicamp.cotuca.studentschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,37 @@ public class MainActivity extends AppCompatActivity {
         btnPost = findViewById(R.id.btnPost);
         btnPut = findViewById(R.id.btnPut);
         btnDelete = findViewById(R.id.btnDelete);
+
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DeleteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
